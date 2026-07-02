6eccb4d5e17373db153b98e4ce0ea1d9cbea0de5
hugopavlak-art <hugo.pavlak@escola.pr.gov.br>
2026-07-02 09:11:33 -0300

    aula 4

--- Script.js
+++ Script.js
@@ -1,22 +1,19 @@
-const botoes = document.querySelectorAll("button");
+ const botoes = document.querySelectorAll("button");
 
-botoes.forEach(function (botao) {
-    // Criamos uma propriedade 'curtiu' diretamente grudada em cada botão individual
-    botao.curtiu = false;
+        botoes.forEach(function (botao) {
+            let curtiu = false;
 
-    botao.addEventListener("click", botaoClicado);
+            botao.addEventListener("click", botaoClicado);
 
-    function botaoClicado() {
-        let texto = botao.querySelector("span");
+            function botaoClicado() {
+                let texto = botao.querySelector("span");
 
-        if (botao.curtiu === false) {
-            texto.textContent++;
-            botao.curtiu = true;
-            botao.style.background = "#ffcccc"; // Opcional: muda a cor ao curtir
-        } else {
-            texto.textContent--;
-            botao.curtiu = false;
-            botao.style.background = "#ffffff"; // Opcional: volta à cor normal
-        }
-    }
-});
+                if (curtiu === false) {
+                    texto.textContent++;
+                    curtiu = true;
+                } else {
+                    texto.textContent--;
+                    curtiu = false;
+                }
+            }
+        });
