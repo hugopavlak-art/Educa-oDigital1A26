3dcfe21ab1172d5bd1573705c8dbdf5ae624554f
hugopavlak-art <hugo.pavlak@escola.pr.gov.br>
2026-07-30 09:25:09 -0300

    aula 7

--- Script.js
+++ Script.js
@@ -24,4 +24,9 @@ btnTemaEscuro.addEventListener("click", mudaTema);
 
 function  mudaTema() {
     const corpoPagina = document.body;
+if (corpoPagina.classList.contains("tema-escuro")) {
+    corpoPagina.classList.remove("tema-escuro");
+} else {
+    corpoPagina.classList.add("tema-escuro");
+    }
 }
